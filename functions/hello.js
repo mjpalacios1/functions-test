0a469a48e3727e56e0c6a97b2932ff9825f5107c
Melany Palacios <38506160+mjpalacios1@users.noreply.github.com>
2021-05-12 11:25:57 -0500

    test2

--- functions/hello.js
+++ functions/hello.js
@@ -1,11 +1,38 @@
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
-  return {
-    headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
-    },
-    statusCode: 200,
-    body: `Hello ${subject}!`
-  }
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+const md5 = require("md5");
+
+mailchimp.setConfig({
+  apiKey: "3ccb284bebb7cdcc848eb6f2d87a7adb-us1",
+  server: "us1",
+});
+
+const email = "mjpc_10@hotmail.com";
+const listId = "2387a9c0f9";
+const subscriberHash = md5(email.toLowerCase());
+
+async function run() {
+  const response = await mailchimp.lists.updateListMemberTags(
+    listId,
+    subscriberHash,
+    {
+      tags: [
+        [
+          {
+            name: "test",
+            status: "active",
+          },          
+        ],
+      ],
+    }
+  );
+
+  console.log(
+    `The return type for this endpoint is null, so this should be true: ${
+      response === null
+    }`
+  );
+}
+
+run();
 }
