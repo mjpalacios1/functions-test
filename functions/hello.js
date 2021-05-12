3deebb76be4cdf30e9b1a3e73db15e2d9d3c2819
Melany Palacios <38506160+mjpalacios1@users.noreply.github.com>
2021-05-12 12:18:51 -0500

    .

--- functions/hello.js
+++ functions/hello.js
@@ -10,7 +10,7 @@ mailchimp.setConfig({
 const email = "mjpalacios0205@gmail.com";
 const listId = "2387a9c0f9";
 const subscriberHash = md5(email.toLowerCase());
-
+console.log(subscriberHash);
 async function run() {
   const response = await mailchimp.lists.updateListMemberTags(
     listId,
