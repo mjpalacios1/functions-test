3d69fac9748a607c1d0094d1865819eba05d012a
Melany Palacios <38506160+mjpalacios1@users.noreply.github.com>
2021-05-12 09:48:00 -0500

    Added headers

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,10 @@
 exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
   return {
+    headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+    },
     statusCode: 200,
     body: `Hello ${subject}!`
   }
