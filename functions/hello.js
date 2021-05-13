e8e6969f79bb2aaf24f6f4c332430f0714d93720
Melany Palacios <38506160+mjpalacios1@users.noreply.github.com>
2021-05-13 11:17:13 -0500

    test3

--- functions/hello.js
+++ functions/hello.js
@@ -1,36 +1,69 @@
-exports.handler = async event => {
-const mailchimp = require("@mailchimp/mailchimp_marketing");
-const md5 = require("md5");
+const axios = require('axios');
+var crypto = require('crypto');
 
-mailchimp.setConfig({
-  apiKey: "3ccb284bebb7cdcc848eb6f2d87a7adb-us1",
-  server: "us1",
-});
+const apiRoot = 'https://us1.api.mailchimp.com/3.0/lists/2387a9c0f9/members/';
 
-const email = "mjpalacios0205@gmail.com";
-const listId = "2387a9c0f9";
-async function run() {
-  const response = await mailchimp.lists.updateListMemberTags(
-    listId,
-    "b3744e266ef5c3c678928dfaf87ab833",
-    {
-      tags: [
-        [
-          {
-            name: "test",
-            status: "active",
-          },          
-        ],
-      ],
+
+exports.handler = async (event, context) => {
+  try {
+    const email ="mjpc_10@hotmail.com";
+    if(!email) {
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTION",
+        },
+        body: 'email query paramter required'
+      };
     }
-  );
 
-  console.log(
-    `The return type for this endpoint is null, so this should be true: ${
-      response === null
-    }`
-  );
-}
+    let emailhash = crypto.createHash('md5').update(email).digest("hex");
 
-run();
-}
+    return axios({
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTION",
+      },
+      method: 'put',
+      url: apiRoot + emailhash,
+      data:{
+        email_address:email,
+        status:'subscribed'
+      },
+      auth: {
+        'username': 'mjpalacios1',
+        'password': '3ccb284bebb7cdcc848eb6f2d87a7adb-us1'
+      },
+
+    }).then(res => {
+      return {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTION",
+        },
+        statusCode:200,
+        body: JSON.stringify(res.data),
+      }
+    })
+        .catch(err => {
+          console.log('returning from here', err.response.data.detail);
+          return {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "Content-Type",
+              "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE,OPTION",
+            },
+            statusCode: 500,
+            body: JSON.stringify(err.response.data),
+          };
+        });
+
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+
+};
